1d72ffc9bd1542b052a0767bc18ee4ea78c0425d
Tawsif <tawsif7492@gmail.com>
2025-07-21 06:38:26 +0000

    Nah, still doesn't work.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,7 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  safelist: [
-    'theme-rose',
-    'theme-green',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
