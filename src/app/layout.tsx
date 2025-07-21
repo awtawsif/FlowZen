cad7efd007aa2d5b47aef0cd6da9648b05cc59fd
Tawsif <tawsif7492@gmail.com>
2025-07-21 06:42:24 +0000

    The theme issue is still not fixed, only the light and dark theme works.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
-          attribute="class"
+          attribute="data-theme"
           defaultTheme="light"
           enableSystem
           disableTransitionOnChange
